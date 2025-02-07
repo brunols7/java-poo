bfb5cc0498c7c25a272660ea079536ee44326e96
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2025-02-06 21:44:24 -0300

    Aula 001 - Métodos

--- Aula 001/src/Carro.java
+++ Aula 001/src/Carro.java
@@ -3,6 +3,41 @@ public class Carro{
     String cor = "vermelho";
     int ano = 1973;
     double potencia = 2.0;
-    boolean funcionando = false;
+    boolean funcionando = true;
+    double combustivel = 15;
 
+    // void = Metodo que não tem retorno
+    // metodo de mostrar as informaçoes do carro
+    void mostraInfo(){
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Cor do carro: " + cor);
+        System.out.println("Ano do carro: " + ano);
+        System.out.println("Potência do carro: " + potencia);
+        if(funcionando){
+            System.out.println("O carro está funcionando!");
+        }else {
+            System.out.println("O carro está quebrado!");
+        }
+    }
+
+    // metodo de andar (so vai andar se estiver funcionando)
+    void andar(){
+        System.out.println("=-=-=-=-=-=-=-=-=");
+        if(funcionando){
+            System.out.println("Carro começou a andar...");
+            if(combustivel > 0){
+                combustivel -= 7;
+                System.out.println("Carro gastou 7 litros de combustivel e começou a andar...");
+            }else{
+                System.out.println("Carro está sem gasolina para continuar andando...");
+            }
+        }else{
+            System.out.println("Carro não consegue andar porque está quebrado.");
+        }
+    }
+
+    void abastecer(){
+        combustivel = 25;
+        System.out.println("Carro está abastecendo...");
+    }
 }
--- Aula 001/src/Main.java
+++ Aula 001/src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        // Parte 1 - Classes e Atributos
         System.out.println("Hello World!");
         Carro c1 = new Carro();
         System.out.println("Cor do Carro 1: " + c1.cor);
@@ -11,11 +13,35 @@ public class Main {
         Carro c2 = new Carro();
         c2.ano = 2015;
         c2.potencia = 2.0;
+        c2.combustivel = 6.5;
         c2.cor = "preto";
         System.out.println("---------------------------------");
         System.out.println("Cor do Carro 2: " + c2.cor);
         System.out.println("Ano do Carro 2: " + c2.ano);
         System.out.println("Potência do Carro 2: " + c2.potencia);
+
+        // Parte 2 - Métodos
+        Carro c3 = new Carro();
+        c3.ano = 2024;
+        c3.combustivel = 6.3;
+        c3.potencia = 2.6;
+        c3.cor = "amarelo";
+        c3.funcionando = false;
+
+        //c1.mostraInfo();
+        //c2.mostraInfo();
+        c3.mostraInfo();
+
+        c1.andar();
+        c2.andar();
+        c3.andar();
+        System.out.println("=-=-=-=-=-=-=-=-=-=");
+        for(int i = 0; i < 5; i++){
+            c1.andar();
+        }
+        c1.abastecer();
+        c1.andar();
+
     }
 }
 
